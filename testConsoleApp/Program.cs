a9263a46eda3c451acc4cd0cf841da9ab94baac6
kato yuki <makeshiftyk@gmail.com>
2018-09-23 18:20:06 +0900

    ファーストコミット

---
+++ testConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                Console.WriteLine("Args:");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(string.Format("args[{0}]: {1}", i, args[i]));
+                }
+            }
+            else
+            {
+                Console.WriteLine("no Args.");
+            }
+        }
+    }
+}
